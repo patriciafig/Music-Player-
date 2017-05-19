a54dfd4ce011b9cba6f86da4ddb7312c6c2777a0
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-05-19 14:18:47 -0600

    Initial Commit

---
+++ My Music Player/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  My Music Player
+//
+//  Created by Patricia on 5/19/17.
+//  Copyright © 2017 Patricia Figueroa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
