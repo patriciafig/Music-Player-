708e8f1faa18b6858332b2bde49721c59e7a7c85
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-05-24 10:15:40 -0600

    adding a function that searches for the song

--- My Music Player/FirstViewController.swift
+++ My Music Player/FirstViewController.swift
@@ -14,12 +14,19 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // instead of making an array with all the songs we want to be able to have the application search for a song and display it in the table view, for this I created a function
+    
+    func GetSongName()   //get the song name from the folder 
+    {
+    
+    }
+    
+    
+    
 }
 
