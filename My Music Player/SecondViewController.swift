fed5fd28fadde083fadaf9208914ceb05d732fe3
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-05-25 11:16:20 -0600

    pause and play button working

--- My Music Player/SecondViewController.swift
+++ My Music Player/SecondViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var LABEL: UILabel!          // song title
+    @IBOutlet weak var MyImage: UIImageView!    // album cover
+    
+    @IBAction func play(_ sender: Any)           //play button
+    {
+        if audioPlayer.isPlaying == false
+        {
+            audioPlayer.play()
+        }
+    }
+    
+    @IBAction func pause(_ sender: Any)    //pause button
+    {
+        if audioPlayer.isPlaying
+        {
+        audioPlayer.pause()
+        }
+    }
+    
+    @IBAction func prev(_ sender: Any)     // prev button
+    {
+    }
+    @IBAction func next(_ sender: Any)   // next button
+    {
+    }
+    
+    @IBAction func slider(_ sender: UISlider)   // slider to adjust the volume 
+    {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
